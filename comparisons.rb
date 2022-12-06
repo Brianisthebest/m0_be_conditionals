26106e32e8e4355fdd8afc30de3b6813b9f2f59f
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2022-12-06 15:11:56 -0700

    Edit comparisons.rb

--- comparisons.rb
+++ comparisons.rb
@@ -22,24 +22,31 @@ puts "Is number_teachers greater than number_students?", number_teachers > numbe
 # this should print: "Is numberTeachers greater than numberStudents?" false
 
 # YOU DO: print the result of the comparison: is number_teachers less than number_students?
+puts "is number_teachers less than number_students?", number_teachers < number_students
 # this should print: true
 
 # YOU DO: print the result of the comparison: is number_teachers equal to string_teachers?
+puts "number_teachers equal to string_teachers?", number_teachers == string_teachers
 # this should print: false
 
 # YOU DO: print the result of the comparison: is number_teachers not equal to number_students?
+puts "number_teachers not equal to number_students?", number_teachers != number_students
 # this should print: true
 
 # YOU DO: print the result of the comparison: is number_students greater than or equal to 20?
+puts "is number_students greater than or equal to 20?", number_students >= 20
 # this should print: true
 
 # YOU DO: print the result of the comparison: is number_students greater than or equal to 21?
+puts "is number_students greater than or equal to 21?", number_students >= 21
 # this should print: false
 
 # YOU DO: print the result of the comparison: is number_students less than or equal to 20?
+puts "is number_students less than or equal to 20?", number_students <= 20
 # this should print: true
 
 # YOU DO: print the result of the comparison: is number_students less than or equal to 21?
+puts "is number_students less than or equal to 21?", number_students <= 21
 # this should print: true
 
 
@@ -53,21 +60,24 @@ puts "Is number_teachers greater than number_students?", number_teachers > numbe
 # Make sure YOU can explain it that way!
 
 puts 4 < 9
-#YOU DO: Explain.
+#YOU DO: This will print out the results of is 4 lesser than 9, which is true, so it should print out true.
 
 books = 3
 puts 4 < books
-# YOU DO: Explain.
+# YOU DO: Books is being defined as the value of 3. It's then printing is 4 is lesser than 3 since that's 
+# the value books has been given, which would print out a false statement.
 
 friends = 6
 siblings = 2
 puts friends > siblings
-# YOU DO: Explain.
+# YOU DO: The variable friends is given the value of 6. The variable siblings is given the value of 2.
+# We're then looking to see if the value of friends is greater than 2, which it is, so it should print out true.
 
 attendees = 9
 meals = 8
 puts attendees != meals
-# YOU DO: Explain.
+# YOU DO: The variable attendees is given the vaule of 9. The variable meals if given the value of 8.
+# We're then printing is 9 is not equal to 8, which they aren't so it should print out a true statement.
 
 
 #-------------------
@@ -87,18 +97,18 @@ loves_to_play = true
 loves_dog_park = false
 loves_treats = true
 age = 1
-
+puts "dog"
 # YOU DO:
 # Determine if the dog loves to play and loves treats
-
+puts loves_to_play && loves_treats
 
 # Determine if the dog loves to play and loves the dog park
-
+puts loves_to_play && loves_dog_park
 
 # Determine if the dog loves to play or loves the dog park
-
+puts loves_to_play || loves_dog_park
 
 # Determine if the dog loves to play and is a puppy
-
+puts loves_to_play && age
 # What did your final line of code evaluate to? Why do you think that is? Explain.
-# ANSWER:
+# ANSWER: 1. age isn't a logical operator so it wasn't checking a true or false statement it printed age which is 1.
